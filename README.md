64a228347b81003daaec7049d3df16d1bf8420b0
Gabriel Weitoft (gwe) <gabriel.weitoft@qlik.com>
2018-05-30 16:05:21 +0200

    A starting point

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# core-authorization
-[EXPERIMENTAL] Example of how to set up authorization in Qlik Core
+# [EXPERIMENTAL] Core Authorization
+
+This repo consists of two parts:
+1. Three Qlik Associative Engines set up with different access rules. 
+2. A Set of predefined JWTs.
+2. A set of test cases that use illustrate how the different combinations b.
+
+## Running
+
+**Note: You need Go installed and the repo located correctly on the Go path
+
+
+Start the engines using docker-compose:
+
+```bash
+docker-compose up -d
+```
+
+To see how the engines Run the test cases
+
+```bash
+docker-compose up -d
+```
+
+
---
+++ go/basic_access_test.go
@@ -0,0 +1,23 @@
+package tests
+
+import (
+	"testing"
+	"github.com/dgrijalva/jwt-go"
+	"github.com/stretchr/testify/assert"
+)
+
+
+func TestThatTheCountryListBoxIsBlockedWithoutJwt(t *testing.T) {
+	err := accessEngineAppObjectWithJwt("ws://localhost:9071", "Shared-Africa-Urbanization.qvf", "eVTNPmv", nil)
+	assert.Error(t, err)
+}
+
+func TestThatTheCountryListBoxIsAccessibleInEngine1WithJwt(t *testing.T) {
+
+	jwtClaims := jwt.MapClaims{
+		"sub":    "user1",
+		"groups": []string{"A", "B"},
+	}
+	err := accessEngineAppObjectWithJwt("ws://localhost:9071", "Shared-Africa-Urbanization.qvf", "eVTNPmv", jwtClaims)
+	assert.Nil(t, err)
+}
